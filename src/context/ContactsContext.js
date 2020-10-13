312460df0b8249550007d811d33e5b83d84a0ba8
Will Lorand <will@milemarker3.org>
2020-10-12 20:38:03 -0700

    v1.0 reducer file, touch of view file

--- src/context/ContactsContext.js
+++ src/context/ContactsContext.js
@@ -4,5 +4,52 @@ import React, { useReducer, createContext } from 'react';
 export const ContactsContext = createContext();
 
 // set initial state
+const initialState = {
+  contacts: [
+    { id: '098', name: 'Diana Prince', email: 'diana@us.army.mil' },
+    { id: '099', name: 'Bruce Wayne', email: 'bruce@batmail.com' },
+    { id: '100', name: 'Clark Kent', email: 'clark@metropolitan.com' },
+  ],
+  loading: false,
+  error: null,
+};
 
-// export a
+// Contacts Reducer
+const contactsReducer = (state, action) => {
+  // typical switch statement to handle diff cases
+  switch (action.type) {
+    case 'ADD_CONTACT':
+      return {
+        contacts: [...state.contacts, action.payload], // add a contact via array with spread existing
+      };
+    case 'DEL_CONTACT':
+      return {
+        contacts: state.contacts.filter(
+          (contact) => contact.id !== action.payload
+        ), // DEL via filter
+      };
+    case 'START':
+      return {
+        loading: true,
+      };
+    case 'COMPLETE':
+      return {
+        loading: false,
+      };
+    default:
+      throw new Error();
+  }
+};
+
+// export a Context Provider
+export const ContactContextProvider = (props) => {
+  // create inital state and a dispatch method to update it via useReducer
+  const [state, dispatch] = useReducer(contactsReducer, initialState);
+
+  // return a Context Provider
+  return (
+    <ContactContext.Provider value={[state, dispatch]}>
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
